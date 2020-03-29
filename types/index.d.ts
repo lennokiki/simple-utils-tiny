9b5b4352398d9936ee72a83d1a367754b3f2a6f9
liugaozhan <372563106@qq.com>
2020-03-30 01:46:02 +0800

    edit the d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -5,12 +5,11 @@ type Transcases = 'capitalize' | 'upper' | 'lower';
 type Lettertypes = 'upper' | 'lower';
 
 export as namespace sUtils;
-export = sUtils;
 
 /* two object value is equal */
-declare function typeIsEqual (target1: object, target2: object): boolean;
+export declare function typeIsEqual (target1: object, target2: object): boolean;
 /* judge this target type */
-declare namespace typeIs {
+export declare namespace typeIs {
   /* string */
   function isString (targert: any): boolean;
   /* is number */
@@ -41,63 +40,63 @@ declare namespace typeIs {
 
 
 /* trim space */
-declare function stringTrim (string: string, type?: Trimtypes): string;
+export declare function stringTrim (string: string, type?: Trimtypes): string;
 
 /* transform word */
-declare function stringTransCase (string: string, type?: Transcases): string;
+export declare function stringTransCase (string: string, type?: Transcases): string;
 
 /* generate random color */
-declare function stringColor (isRgb?: boolean): string;
+export declare function stringColor (isRgb?: boolean): string;
 
 /* transform 16 color to rgb */
-declare function stringColor16ToRgb (string: string): string;
+export declare function stringColor16ToRgb (string: string): string;
 
 /* transform rgb to 16 color */
-declare function stringColorRgbTo16 (string: string): string;
+export declare function stringColorRgbTo16 (string: string): string;
 
 /* transform number value to word value */
-declare function stringNumToLetter (length: number, type?: Lettertypes): string;
+export declare function stringNumToLetter (length: number, type?: Lettertypes): string;
 
 
 
 /* is integer */
-declare function rgIsInteger (number: any, type?: Numbertypes): boolean;
+export declare function rgIsInteger (number: any, type?: Numbertypes): boolean;
 
 /* is float */
-declare function rgIsFloat (number: any, type?: Numbertypes): boolean;
+export declare function rgIsFloat (number: any, type?: Numbertypes): boolean;
 
 /* is phone */
-declare function rgIsPhone (string: any): boolean;
+export declare function rgIsPhone (string: any): boolean;
 
 /* is tel */
-declare function rgIsTel (string: any): boolean;
+export declare function rgIsTel (string: any): boolean;
 
 /* is tel or mobile */
-declare function rgIsTelOrMobile (string: any): boolean;
+export declare function rgIsTelOrMobile (string: any): boolean;
 
 /* is chinese */
-declare function rgIsChinese (string: any): boolean;
+export declare function rgIsChinese (string: any): boolean;
 
 /* is email */
-declare function rgIsEmail (string: any): boolean;
+export declare function rgIsEmail (string: any): boolean;
 
 /* is id card */
-declare function rgIsID (string: any): boolean;
+export declare function rgIsID (string: any): boolean;
 
 /* is url */
-declare function rgIsUrl (string: any): boolean;
+export declare function rgIsUrl (string: any): boolean;
 
 /* is pc */
-declare function rgIsPC (string: any): boolean;
+export declare function rgIsPC (string: any): boolean;
 
 /* tes current browser type */
-declare function rgBrowserType (test360?: boolean): string;
+export declare function rgBrowserType (test360?: boolean): string;
 
 /* add or delete thousand mark */
-declare function rgThousandMark (string: any, type?: Thousandmark): string;
+export declare function rgThousandMark (string: any, type?: Thousandmark): string;
 
 /* allow inputing integer */
-declare function rgAllowInteger (string: string | number, length?: number, type?: Numbertypes): string;
+export declare function rgAllowInteger (string: string | number, length?: number, type?: Numbertypes): string;
 
 /* allow inputing float */
-declare function rgAllowFloat (string: string | number, floatLength?: number, type?: Numbertypes, integerLength?: number): string;
+export declare function rgAllowFloat (string: string | number, floatLength?: number, type?: Numbertypes, integerLength?: number): string;
